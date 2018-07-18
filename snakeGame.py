a1ff774e1c592c3d1bc07e73e6b7bc6c94867104
Allex Fernando Vissoci <allex.vissoci@meta.com.br>
2018-07-18 14:04:04 -0300

    create game over function

--- snakeGame.py
+++ snakeGame.py
@@ -7,7 +7,7 @@ import time
 
 check_errors = pygame.init()[1]
 
-# check for initializing errors
+# Check for initializing errors
 if check_errors > 0:
     print("(!) Had {0} initializing errors, exiting...".format(check_errors))
     sys.exit(-1)
@@ -17,8 +17,34 @@ playSourface = pygame.display.set_mode((720, 460))
 pygame.display.set_caption("Snake Game!")
 
 # Colors
-red = pygame.Color(255, 0, 0) # gameover
-blue = pygame.Color(0, 0, 255) # snake
-black = pygame.Color(0, 0, 0) # score
-white = pygame.Color(255, 255, 255) # background
-brown = pygame.Color(165, 42, 42) # food
+red = pygame.Color(255, 0, 0) #gameover
+blue = pygame.Color(0, 0, 255) #snake
+black = pygame.Color(0, 0, 0) #score
+white = pygame.Color(255, 255, 255) #background
+brown = pygame.Color(165, 42, 42) #food
+
+# FPS controller
+fpsController = pygame.time.Clock()
+
+# Important variables
+snakePos = [100, 50]
+snakeBody = [[100, 50], [90, 50], [80, 50]]
+
+foodPos = [random.randrange(1, 72)*10, random.randrange(1, 46)*10]
+foodSpawn = True
+
+direction = 'rigth'
+changeTo = direction
+
+
+# Game over function
+def gameOver():
+    myFont = pygame.font.SysFont('monaco', 72)
+    GOsurf = myFont.render('Game over!', True, red)
+    GOrect = GOsurf.get_rect()
+    GOrect.midtop = (360, 15)
+    playSourface.blit(GOsurf, GOrect)
+    pygame.display.flip()
+    time.sleep(5)
+    pygame.quit()
+    sys.exit()
