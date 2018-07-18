e6de55854674fbaf46747bf818109657169af201
Allex Fernando Vissoci <allex.vissoci@meta.com.br>
2018-07-18 11:14:46 -0300

    Creating a player surface

--- snakeGame.py
+++ snakeGame.py
@@ -7,8 +7,12 @@ import time
 
 check_errors = pygame.init()[1]
 
+# check for initializing errors
 if check_errors > 0:
     print("(!) Had {0} initializing errors, exiting...".format(check_errors))
     sys.exit(-1)
-else:
-    print("(+) PyGame successfully initialized!")
+
+# Play surface
+playSourface = pygame.display.set_mode((720, 460))
+pygame.display.set_caption("Snake Game!")
+time.sleep(5)
